58a6afb98709b980dd907db0deebad67cbb64353
Kayla <kay_kay1424@yahoo.com>
2021-03-04 14:23:03 -0600

    add array of tv shows and display them on home page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
 $(document).ready(function() {
-    alert('Hello world');
-    let favoriteFood = 'mac and cheese';
-    document.write(favoriteFood);
+    let tvShowsList = [
+        {
+            name: 'The Voice',
+            genres: ['Music'],
+            rating: 6.8
+        },
+        {
+            name: 'Superstore',
+            genres: ['Comedy'],
+            rating: 7.1
+        },
+        {
+            name: 'American Ninja Warrior',
+            genres: [
+                'Action',
+                'Sports'
+            ],
+            rating: 6.8
+        }
+    ];
+    tvShowsList.map(({name,rating,genres}) => {
+        let html = `
+            <div class="show">
+                <h2>${name}</h2>
+                <p><b>Rating:</b> ${rating}</p>
+                <p><b>Genres:</b> </p>
+                <ul>
+        `;
+        for (let i = 0; i < genres.length; i++) {
+            html += `<li>${genres[i]}</li>`;
+        }
+        html += `
+                </ul>
+            </div>
+            <hr>
+        `;
+        $('#tv-shows').append(html);
+    });
 });
