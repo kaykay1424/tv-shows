884f2a2f806935cbbdb785e07e599d80edc09022
Kayla <kay_kay1424@yahoo.com>
2021-03-09 11:33:04 -0600

    add for loop and conditional logic for show ratings

--- js/main.js
+++ js/main.js
@@ -19,17 +19,32 @@ $(document).ready(function() {
             rating: 6.8
         }
     ];
+
+    for (let i = 0; i < tvShowsList.length; i++) {
+        let show = tvShowsList[i];
+        console.log(show.name + ` (rating: ${show.rating}) ${show.rating > 7 ? " -Wow, that's a great show!": ''}`);
+    }
+
     tvShowsList.map(({name,rating,genres}) => {
         let html = `
             <div class="show">
                 <h2>${name}</h2>
-                <p><b>Rating:</b> ${rating}</p>
+                <p><b>Rating: ${rating}</b>
+        `;
+        
+        if (rating > 7) {
+            html += " -Wow, that's a great show!";
+        }
+                
+        html += `</p>
                 <p><b>Genres:</b> </p>
                 <ul>
         `;
+
         for (let i = 0; i < genres.length; i++) {
             html += `<li>${genres[i]}</li>`;
         }
+
         html += `
                 </ul>
             </div>
