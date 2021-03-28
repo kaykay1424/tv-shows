7295cba1f724f4783aef07f5c7871c8ea548e7ac
Kayla <kay_kay1424@yahoo.com>
2021-03-28 17:38:56 -0500

    add polyfills for promise/fetch, connect app to api, show tv show details in modal

--- js/main.js
+++ js/main.js
@@ -1,35 +1,19 @@
 $(document).ready(function() {
-    let app = (function() {
-        let tvShowsList = [
-            {
-                name: 'The Voice',
-                genres: ['Music'],
-                rating: 6.8
-            },
-            {
-                name: 'Superstore',
-                genres: ['Comedy'],
-                rating: 7.1
-            },
-            {
-                name: 'American Ninja Warrior',
-                genres: [
-                    'Action',
-                    'Sports'
-                ],
-                rating: 6.8
-            }
-        ];
-        let tvShowProps = ['name','genres','rating'];
+    let tvShowsApp = (function() {
+        let tvShowsList = [];
+        let filteredTVShowsList = [];
+        let tvShowProps = ['name','genres','rating','image'];
+        let apiURL = 'https://api.tvmaze.com/shows';
+        let apiSearchURL = 'https://api.tvmaze.com/search/shows?q='
 
         function filter(search) {
             return tvShowsList.filter((show) => {
                 let regex = new RegExp(search,'i');
                 return show.name.match(regex);
-            });
+            });  
         }
 
-        function add(show) {
+        function add(show,filtered) {
             let isDataValid = true;
 
             if (typeof show === 'object') {
@@ -42,18 +26,108 @@ $(document).ready(function() {
             } 
 
             if (isDataValid) {
-                tvShowsList.push(show);
+                if (filtered) {
+                    filteredTVShowsList.push(show);
+                } else {
+                    tvShowsList.push(show);
+                }                
             }            
         }
 
-        function addListItem({name,genres,rating}) {
+        function addListItem(show) {
             let list = document.querySelector('#tv-shows-list');
             let listItem = document.createElement('li');
-            let button = document.createElement('button');
-            let detailsDiv = document.createElement('div');
+            let button = document.createElement('button');            
+            button.classList.add('btn');
+            button.addEventListener('click', function() {
+                showDetails(show);
+            });
+            button.innerText = show.name;            
+            listItem.classList.add('show');
+            listItem.appendChild(button);
+            list.appendChild(listItem);
+        }
+
+        function getAll(filtered) {
+           if (filtered) {
+            return filteredTVShowsList;
+           } 
+
+           return tvShowsList; 
+        }
+
+        function loadList(search) {
+            let url = search ? apiSearchURL+search : apiURL; 
+            showLoadingMessage();
+                        
+            return fetch(url).then(function(response) {
+                return response.json();
+            }).then(function(results) {
+                results.forEach((result) => {
+                    if (!result.name) { // if url is search url, show properties from results are 1 level deeper in array
+                        result = result.show;
+                    }
+                    
+                    let show = {
+                        name: result.name,
+                        genres: result.genres,
+                        rating: result.rating.average,
+                        image: result.image.medium
+                    }
+                    
+                    search ? add(show,true) : add(show);  
+                });
+                hideLoadingMessage();
+            }).catch(function(error) {
+                console.log(error)
+                hideLoadingMessage();
+            });
+        }
+
+        function hideLoadingMessage() {
+            let loadingMessageDiv = document.querySelector('.loading-message');
+            let tvShowsDiv = document.getElementById('tv-shows');
+            tvShowsDiv.removeChild(loadingMessageDiv);
+        }
+
+        function showLoadingMessage() {
+            let loadingMessageDiv = document.createElement('div');
+            loadingMessageDiv.innerHTML = 'Loading &#8230;'
+            loadingMessageDiv.classList.add('loading-message');
+            let tvShowsDiv = document.getElementById('tv-shows');
+            let tvShowsList = document.getElementById('tv-shows-list');
+            tvShowsDiv.insertBefore(loadingMessageDiv,tvShowsList);
+        }
+
+        function hideDetails() {
+            let detailsDiv = document.querySelector('.details');
+            let containerDiv = document.querySelector('.container');
+            document.body.removeChild(detailsDiv);
+            containerDiv.style.opacity = 1;
+        }
+
+        function showDetails({name,genres,rating,image}) {
+            // let clickedButton = event.target;
+            // clickedButton.nextElementSibling.classList.toggle('visible');
+            // clickedButton.classList.toggle('clicked');
+            // clickedButton.blur();
+            let detailsDiv = document.querySelector('.details');
+            let containerDiv = document.querySelector('.container');
+
+            if (detailsDiv) {
+                document.body.removeChild(detailsDiv);
+            }
+
+            detailsDiv = document.createElement('div');
             let detailsHtml = `
-                <h2>${name}</h2>
-                <p><b>Rating:</b> ${rating}
+                <div class="details__container">
+                    <span class="close">&#10008;</span>
+                    <div>
+                        <img src="${image}" alt="image of ${name}" />
+                    </div>
+                    <div>
+                        <h2>${name}</h2>
+                        <p><b>Rating:</b> ${rating ? rating : 'N/A'}
             `;
         
             if (rating > 7) {
@@ -61,64 +135,60 @@ $(document).ready(function() {
             }
                 
             detailsHtml += `</p>
-                <p><b>Genres:</b> </p>
-                <ul>
+                <p><b>Genres:</b>
             `;
+            
+            if (genres.length === 0) {
+                detailsHtml += `N/A</p>`;
+            } else {
+                detailsHtml += `</p><ul>`;
+                
+                for (let i = 0; i < genres.length; i++) {
+                    detailsHtml += `<li>${genres[i]}</li>`;
+                }
 
-            for (let i = 0; i < genres.length; i++) {
-                detailsHtml += `<li>${genres[i]}</li>`;
+                detailsHtml += `</ul>`; 
             }
 
-            detailsHtml += `
-                </ul>
+            detailsHtml += `                  
+                    </div>                    
+                </div>
             `;
-            button.classList.add('btn');
-            button.addEventListener('click', function(event) {
-                showDetails(event);
-            });
-            button.innerText = name;
             detailsDiv.classList.add('details');
             detailsDiv.innerHTML = detailsHtml;
-            listItem.classList.add('show');
-            listItem.appendChild(button);
-            listItem.appendChild(detailsDiv)
-            list.appendChild(listItem);
+            document.body.appendChild(detailsDiv);
+            detailsDiv.querySelector('.close').addEventListener('click', hideDetails);
+            containerDiv.style.opacity = 0.5;
         }
 
-        function getAll() {
-            return tvShowsList;
-        }
-
-        function showDetails(event) {
-            let clickedButton = event.target;
-            clickedButton.nextElementSibling.classList.toggle('visible');
-            clickedButton.classList.toggle('clicked');
-            clickedButton.blur();
-        }
-        
         return {
             add,
             addListItem,
             filter,
+            loadList,
             getAll
         };
     })();
  
     function displayShows(shows) {
         $('#tv-shows-list').html('');
-        shows.forEach((show) => app.addListItem(show));
+        shows.forEach((show) => tvShowsApp.addListItem(show));
     }
 
-    displayShows(app.getAll());
+    tvShowsApp.loadList().then(function() {
+        displayShows(tvShowsApp.getAll(false));
+    });
 
     $('#search-form').submit((e) => {
         e.preventDefault();
         let search = $('#search').val();
-        displayShows(app.filter(search));
+        tvShowsApp.loadList(search).then(function() {
+            displayShows(tvShowsApp.getAll(true));
+        });
     });
 
     $('#clear-search').click(() => {
         $('#search').val('');
-        displayShows(app.getAll());
+        displayShows(tvShowsApp.getAll(false));
     });    
 });
